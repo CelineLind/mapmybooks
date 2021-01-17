54608c8894718df37f57fbced4391672c8031298
Celine <celinelindeque@gmail.com>
2021-01-17 15:41:17 +1000

    working search functionality

--- dist/mapmybooks.e31bb0bc.js
+++ dist/mapmybooks.e31bb0bc.js
@@ -118,6 +118,21 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   return newRequire;
 })({"index.js":[function(require,module,exports) {
+var allBooks = [{
+  title: "Book One",
+  author: "Author One",
+  ISBN: "One"
+}, {
+  title: "Book Two",
+  author: "Author Two",
+  ISBN: "Two"
+}];
+var currentBook = {
+  title: "new",
+  author: "new",
+  ISBN: "new"
+};
+
 (function (win, doc) {
   var olview = new ol.View({
     center: [0, 0],
@@ -127,7 +142,8 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   });
   var baseLayer = new ol.layer.Tile({
     source: new ol.source.OSM()
-  });
+  }); // new map
+
   var map = new ol.Map({
     target: doc.querySelector('#map'),
     view: olview,
@@ -150,119 +166,10 @@ parcelRequire = (function (modules, cache, entry, globalName) {
     window.setTimeout(function () {
       popup.show(evt.coordinate, evt.address.formatted);
     }, 3000);
+    allBooks.push(currentBook);
+    console.log(allBooks);
   });
 })(window, document);
-/*import 'ol/ol.css';
-import {Map, View} from 'ol';
-import TileLayer from 'ol/layer/Tile';
-import OSM from 'ol/source/OSM';
-
-((win, doc) => {
-  const olview = new ol.View({
-    center: [0, 0],
-    zoom: 3,
-    minZoom: 2,
-    maxZoom: 20,
-  });
-
-  const baseLayer = new ol.layer.Tile({
-    source: new ol.source.OSM(),
-  });
-  const map = new ol.Map({
-    target: doc.querySelector('#map'),
-    view: olview,
-    layers: [baseLayer],
-  });
-  const popup = new ol.Overlay.Popup();
-
-  // Instantiate with some options and add the Control
-  const geocoder = new Geocoder('nominatim', {
-    provider: 'osm',
-    targetType: 'text-input',
-    lang: 'en',
-    placeholder: 'Search for ...',
-    limit: 5,
-    keepOpen: false,
-  });
-
-  map.addControl(geocoder);
-  map.addOverlay(popup);
-
-  // Listen when an address is chosen
-  geocoder.on('addresschosen', (evt) => {
-    window.setTimeout(() => {
-      popup.show(evt.coordinate, evt.address.formatted);
-    }, 3000);
-  });
-})(window, document);
-
-/*
-var olview = new ol.View({ center: [0, 0], zoom: 2 }),
-    baseLayer = new ol.layer.Tile({ source: new ol.source.OSM() }),
-    map = new ol.Map({
-      target: document.getElementById('map'),
-      view: olview,
-      layers: [baseLayer]
-    });
-    
-// popup
-var popup = new ol.Overlay.Popup();
-map.addOverlay(popup);
-
-var geocoder = new Geocoder('nominatim', {
-  provider: 'mapquest',
-  key: '__some_key__',
-  lang: 'pt-BR', //en-US, fr-FR
-  placeholder: 'Search for ...',
-  targetType: 'text-input',
-  limit: 5,
-  keepOpen: true
-});
-map.addControl(geocoder);
-
-geocoder.on('addresschosen', function(evt){
-  var feature = evt.feature,
-      coord = evt.coordinate,
-      address = evt.address;
-  // some popup solution
-  content.innerHTML = '<p>'+ address.formatted +'</p>';
-  overlay.setPosition(coord);
-});
-
-/*
-//Instantiate with some options and add the Control
-var geocoder = new Geocoder('nominatim', {
-  provider: 'osm',
-  lang: 'en',
-  placeholder: 'Search for ...',
-  limit: 5,
-  debug: false,
-  autoComplete: true,
-  keepOpen: true
-});
-map.addControl(geocoder);
-  
-//Listen when an address is chosen
-geocoder.on('addresschosen', function (evt) {
-	console.info(evt);
-  window.setTimeout(function () {
-    popup.show(evt.coordinate, evt.address.formatted);
-  }, 3000);
-});
-
-/*
-const map = new Map({
-  target: 'map',
-  layers: [
-    new TileLayer({
-      source: new OSM()
-    })
-  ],
-  view: new View({
-    center: [0, 0],
-    zoom: 0
-  })
-});*/
 },{}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -291,7 +198,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55792" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55838" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+
+var allBooks = [
+  {
+    title: "Book One",
+    author: "Author One",
+    ISBN: "One"
+  },
+  {
+    title: "Book Two",
+    author: "Author Two",
+    ISBN: "Two"
+  }
+];
+
+var currentBook = {
+  title: "new",
+  author: "new",
+  ISBN: "new"
+};
+
 ((win, doc) => {
   const olview = new ol.View({
     center: [0, 0],
@@ -9,11 +29,14 @@
   const baseLayer = new ol.layer.Tile({
     source: new ol.source.OSM(),
   });
+
+  // new map
   const map = new ol.Map({
     target: doc.querySelector('#map'),
     view: olview,
     layers: [baseLayer],
   });
+
   const popup = new ol.Overlay.Popup();
 
   // Instantiate with some options and add the Control
@@ -34,119 +57,7 @@
     window.setTimeout(() => {
       popup.show(evt.coordinate, evt.address.formatted);
     }, 3000);
+    allBooks.push(currentBook);
+    console.log(allBooks);
   });
 })(window, document);
-
-
-
-/*import 'ol/ol.css';
-import {Map, View} from 'ol';
-import TileLayer from 'ol/layer/Tile';
-import OSM from 'ol/source/OSM';
-
-((win, doc) => {
-  const olview = new ol.View({
-    center: [0, 0],
-    zoom: 3,
-    minZoom: 2,
-    maxZoom: 20,
-  });
-
-  const baseLayer = new ol.layer.Tile({
-    source: new ol.source.OSM(),
-  });
-  const map = new ol.Map({
-    target: doc.querySelector('#map'),
-    view: olview,
-    layers: [baseLayer],
-  });
-  const popup = new ol.Overlay.Popup();
-
-  // Instantiate with some options and add the Control
-  const geocoder = new Geocoder('nominatim', {
-    provider: 'osm',
-    targetType: 'text-input',
-    lang: 'en',
-    placeholder: 'Search for ...',
-    limit: 5,
-    keepOpen: false,
-  });
-
-  map.addControl(geocoder);
-  map.addOverlay(popup);
-
-  // Listen when an address is chosen
-  geocoder.on('addresschosen', (evt) => {
-    window.setTimeout(() => {
-      popup.show(evt.coordinate, evt.address.formatted);
-    }, 3000);
-  });
-})(window, document);
-
-/*
-var olview = new ol.View({ center: [0, 0], zoom: 2 }),
-    baseLayer = new ol.layer.Tile({ source: new ol.source.OSM() }),
-    map = new ol.Map({
-      target: document.getElementById('map'),
-      view: olview,
-      layers: [baseLayer]
-    });
-    
-// popup
-var popup = new ol.Overlay.Popup();
-map.addOverlay(popup);
-
-var geocoder = new Geocoder('nominatim', {
-  provider: 'mapquest',
-  key: '__some_key__',
-  lang: 'pt-BR', //en-US, fr-FR
-  placeholder: 'Search for ...',
-  targetType: 'text-input',
-  limit: 5,
-  keepOpen: true
-});
-map.addControl(geocoder);
-
-geocoder.on('addresschosen', function(evt){
-  var feature = evt.feature,
-      coord = evt.coordinate,
-      address = evt.address;
-  // some popup solution
-  content.innerHTML = '<p>'+ address.formatted +'</p>';
-  overlay.setPosition(coord);
-});
-
-/*
-//Instantiate with some options and add the Control
-var geocoder = new Geocoder('nominatim', {
-  provider: 'osm',
-  lang: 'en',
-  placeholder: 'Search for ...',
-  limit: 5,
-  debug: false,
-  autoComplete: true,
-  keepOpen: true
-});
-map.addControl(geocoder);
-  
-//Listen when an address is chosen
-geocoder.on('addresschosen', function (evt) {
-	console.info(evt);
-  window.setTimeout(function () {
-    popup.show(evt.coordinate, evt.address.formatted);
-  }, 3000);
-});
-
-/*
-const map = new Map({
-  target: 'map',
-  layers: [
-    new TileLayer({
-      source: new OSM()
-    })
-  ],
-  view: new View({
-    center: [0, 0],
-    zoom: 0
-  })
-});*/
