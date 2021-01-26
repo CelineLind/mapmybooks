e7e566a9b0f5321f9324ec23b3bbee60c29d2030
Celine <celinelindeque@gmail.com>
2021-01-26 13:26:05 +1000

    created faulty react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// import a css
 
-function App() {
+// components
+//import Header from "./components/Header";
+//import Footer from "./components/Footer";
+
+// pages
+import Home from "./pages/Home";
+import CreateNewMap from "./pages/CreateNewMap";
+// Import Map
+// Donations Link
+// My Website
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+
+        {/* the content */}
+        <Switch>
+            <Route exact path="/">
+                <Home />
+            </Route>
+
+            <Route exact path="/create">
+                <CreateNewMap />
+            </Route>
+
+        </Switch>
+
+        <Footer />
+      </div>
+    </Router>
   );
 }
-
-export default App;
---
+++ src/pages/CreateNewMap.js
@@ -0,0 +1,74 @@
+import React, { useState, useEffect } from "react";
+
+////// variables //////
+
+var allBooks = [
+  {
+    title: "A Song Below Water",
+    author: "Author",
+    ISBN: "ISBN One",
+    coord: [-13656028.909742799, 5703798.986450608],
+    address: "<span class=\"gcd-road\">Portland, Metro, Oregon, United States</span>"
+  }
+];
+
+var currentBook = {
+    title: "New",
+    author: "New",
+    ISBN: "New",
+    coord: undefined,
+    address: undefined
+};
+
+///////////////////////
+
+export default function mapPage(){
+  ((win, doc) => {
+    const olview = new ol.View({
+      center: [0, 0],
+      zoom: 3,
+      minZoom: 2,
+      maxZoom: 20,
+    });
+  
+    const baseLayer = new ol.layer.Tile({
+      source: new ol.source.OSM(),
+    });
+  
+    // new map
+    const map = new ol.Map({
+      target: doc.querySelector('#root'), // #map
+      view: olview,
+      layers: [baseLayer],
+    });
+  
+    const popup = new ol.Overlay.Popup(); // the search bar container i think
+  
+    // Instantiate with some options and add the Control
+    const geocoder = new Geocoder('nominatim', {
+      provider: 'osm',
+      targetType: 'text-input',
+      lang: 'en',
+      placeholder: 'Search for a city or country',
+      limit: 5,
+      keepOpen: false,
+    });
+  
+    map.addControl(geocoder); // the search bar text interpreter
+    map.addOverlay(popup); // the search bar container
+  
+    // Listen when an address is chosen
+    geocoder.on('addresschosen', (evt) => {
+      window.setTimeout(() => {
+        popup.show(evt.coordinate, evt.address.formatted); // evt.coordinate is the location coordinate, evt.address.formatted puts the marker text inside <spans>
+      }, 2000);
+      currentBook.coord = evt.coordinate;
+      currentBook.address = evt.address.formatted;
+      allBooks.push(currentBook);
+      console.log(allBooks);
+    });
+  })(window, document);
+
+};
+
+//////////////////////
