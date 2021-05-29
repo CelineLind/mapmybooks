65684a0fe3dd45531b4d6db4944fedfddd34123a
Celine <celinelindeque@gmail.com>
2021-05-29 17:56:07 +1000

    Home Page appears instead of errors for now at least

--- src/App.js
+++ src/App.js
@@ -1,39 +1,31 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-// import a css
+import { render } from '@testing-library/react';
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import Testing from './pages/Testing';
 
-// components
-//import Header from "./components/Header";
-//import Footer from "./components/Footer";
-
-// pages
-import Home from "./pages/Home";
-//import CreateNewMap from "./pages/CreateNewMap";
-import Testing from "./pages/Testing";
-// Import Map
-// Donations Link
-// My Website
-
-export default function App() {
+export default function App(){
   return (
-    <Router>
-      <div className="App">
-        <div id="mapHere"></div>
+    <main>
+      <BrowserRouter>
+        <div className="App">
+          <div id="mapHere"></div>
 
-        {/* the content */}
-        <Switch>
-            <Route exact path="/">
-                <Home /> {/* this error can be ignored as it's just complaining that Home.js is empty */}
-            </Route>
+          {/* the content */}
+          <Switch>
+              <Route exact path="/">
+                  <Home /> {/* this error can be ignored as it's just complaining that Home.js is empty */}
+              </Route>
 
-            <Route exact path="/create">
-                <Testing />
-            </Route>
+              <Route exact path="/create">
+                  <Testing />
+              </Route>
 
-        </Switch>
+          </Switch>
 
-        
-      </div>
-    </Router>
+          
+        </div>
+      </BrowserRouter>
+    </main>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './style.css';
 
-import App from "./App";
+import App from './App';
 
-const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
-  rootElement
+  </BrowserRouter>,
+  document.getElementById("root")
 );
