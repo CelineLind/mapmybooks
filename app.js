9cfb86bf398c313cb635e47e0013f3e8845c3920
Celine <celinelindeque@gmail.com>
2021-10-23 19:46:33 +1000

    logs search to console

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const bookInput = document.querySelector('.booksearch-input');
 const searchButton = document.querySelector('.search-button');
 const bookList = document.querySelector('.book-list-output');
+var bookURL = "https://www.googleapis.com/books/v1/volumes?q=";
 
 // Event Listeners
 searchButton.addEventListener('click', searchForBooks);
@@ -10,19 +11,37 @@ searchButton.addEventListener('click', searchForBooks);
 function searchForBooks(event){
     event.preventDefault(); // prevents the page refreshing on button click and form submitting
 
-    // create todo div
-    const listDiv = document.createElement('div');
-    listDiv.classList.add('book');
+    // empty book list
+    bookList.innerHTML = "";
+    var userSearch = bookInput.value; // what they've searched
 
-    // create li
-    const newBook = document.createElement('li');
-    newBook.innerText = bookInput.value;
-    newBook.classList.add('book-item');
-    listDiv.appendChild(newBook);
+    // search for the book
+    if (userSearch === " " || userSearch === null){
+        // display an error if an empty search
+    }
+    else {
+        // fetch data from bookURL + bookInput.value
+        fetch(bookURL + userSearch)
+            .then((res) => res.json())
+            .then(data => console.log(data));
+    }
 
-    // append to list
-    bookList.appendChild(listDiv);
 
-    // clear todo input value
+    // // create todo div
+    // const listDiv = document.createElement('div');
+    // listDiv.classList.add('book');
+
+    // // search for book according to bookInput text
+
+    // // create li
+    // const newBook = document.createElement('li');
+    // newBook.innerText = bookInput.value; // around here is where the api code will be
+    // newBook.classList.add('book-item');
+    // listDiv.appendChild(newBook);
+
+    // // append to list
+    // bookList.appendChild(listDiv);
+
+    // clear book title input value
     bookInput.value = "";
 }
