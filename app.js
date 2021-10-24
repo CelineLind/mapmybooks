19d0908c10bb7355d437f4d6b82e76b550043d66
Celine <celinelindeque@gmail.com>
2021-10-24 14:28:26 +1000

    books selected are added to list on screen

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const bookOutputList = document.querySelector('.book-list-output');
 var bookURL = "https://www.googleapis.com/books/v1/volumes?q=";
 
 // Event Listeners
+document.addEventListener('DOMContentLoaded', displayBookList);
 searchButton.addEventListener('click', displaySearch);
 bookSearchList.addEventListener('click', addToBookList);
 
@@ -15,16 +16,16 @@ async function displaySearch(event){
 
     // empty book list
     bookSearchList.innerHTML = "";
-    var userSearch = bookInput.value; // what they've searched
+    //var userSearch = bookInput.value; // what they've searched
     var results;
     var objects;
 
     // search for the book
-    if (userSearch === " " || userSearch === null){
+    if (bookInput.value === " " || bookInput.value === null){
         // display an error if an empty search
     }
     else {
-        results = await searchForBooks(userSearch);
+        results = await searchForBooks();
         objects = results.items;
     }
 
@@ -52,21 +53,18 @@ async function displaySearch(event){
 }
 
 // fetches the book details
-function searchForBooks(userSearch){
+function searchForBooks(event){
     // https://developers.google.com/books/docs/v1/using?authuser=2#WorkingVolumes
     // https://stackoverflow.com/questions/11375173/google-books-api-returns-only-10-results
-    console.log(bookURL + 'intitle:' + userSearch); // displays first 10 at the moment, add button that displays next ten, etc
+    console.log(bookURL + 'intitle:' + bookInput.value); // displays first 10 at the moment, add button that displays next ten, etc
     // fetch data from bookURL + bookInput.value
-    return fetch(bookURL + 'intitle:' + userSearch)
+    return fetch(bookURL + 'intitle:' + bookInput.value)
         .then((res) => res.json())
         .then((responseJson) => {return responseJson});
 }
 
 function addToBookList(event){
     const item = event.target;
-
-    console.log("item:");
-    console.log(item.innerText);
 
     // create div
     const outputDiv = document.createElement('div');
@@ -82,10 +80,34 @@ function addToBookList(event){
     saveLocalBookList(item.innerText);
 
     // append to list
-    bookSearchList.appendChild(outputDiv);
+    bookOutputList.appendChild(outputDiv);
 }
 
 function saveLocalBookList(newBook){
+    myBookList = checkBookInLocalStorage();
+    myBookList.push(newBook);
+    localStorage.setItem('myBookList', JSON.stringify(myBookList));
+}
+
+function displayBookList(){
+    myBookList = checkBookInLocalStorage();
+    myBookList.forEach(function(booktitle){
+        // create div
+        const outputDiv = document.createElement('div');
+        outputDiv.classList.add('book-list');
+
+        // create div
+        const newBook = document.createElement('li');
+        newBook.innerText = booktitle;
+        newBook.classList.add('book-output-item');
+        outputDiv.appendChild(newBook);
+
+        // append to list
+        bookOutputList.appendChild(outputDiv);
+    });
+}
+
+function checkBookInLocalStorage(){
     // check if a book already exists in local storage
     let myBookList;
     if(localStorage.getItem('myBookList') === null){
@@ -93,7 +115,5 @@ function saveLocalBookList(newBook){
     } else {
         myBookList = JSON.parse(localStorage.getItem('myBookList'));
     }
-    // add item to myBookList array and save into local storage
-    myBookList.push(newBook);
-    localStorage.setItem('myBookList', JSON.stringify(myBookList));
+    return myBookList;
 }
